ef7d004d4000f3714c69d6924b829e4911a46cc9
patriciaanne98 <147879232+patriciaanne98@users.noreply.github.com>
2025-05-08 07:12:48 +0000

    update

---
+++ slick-demo/assets/js/script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
